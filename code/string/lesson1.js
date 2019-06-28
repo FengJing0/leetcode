c2aa4828bcf3ee30d9f83c819012825bf4332312
FengJing <448195466@qq.com>
2019-06-28 11:22:01 +0800

    .

---
+++ code/string/lesson1.js
@@ -0,0 +1,5 @@
+// export default str => {
+//   let arr = str.split(' ')
+//   return arr.map(item => item.split('').reverse().join('')).join(' ')
+// }
+export default str => str.split(/\s/g).map(item => item.split('').reverse().join('')).join(' ')
